ae2bd4e271b009a0d48c566d36e8dcc711627625
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-03-10 09:17:02 -0500

    initizalize with book_list

--- book_list/src/index.js
+++ book_list/src/index.js
@@ -1,46 +1,19 @@
-import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/seearch_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
 
 
 const API_KEY = "AIzaSyAcqaU2p5x7l_FRU35I94wlld4Ap4tLrfY";
 
 class App extends Component{
   constructor(props){
-    super(props);
-
-    this.state = {
-      videos:[],
-      selectedVideo: null
-    };
-
-    this.videoSearch('surfboards');
-    
+    super(props);  
   }
   
-  videoSearch(term){    
-    YTSearch({key: API_KEY, term:term}, (videos) =>{
-      this.setState({
-        videos :videos,
-        selectedVideo:videos[0] 
-      })  //this.setState({videos: videos})
-    });
-
-  }
+  
   
   render(){
-    const videoSearch = _.debounce((term) =>{this.videoSearch(term) }, 1000)
-
-    return <div>
-              <SearchBar onSearchTermChange={videoSearch}/>
-              <VideoDetail video = {this.state.selectedVideo}/>
-              <VideoList 
-              onVideoSelect = { selectedVideo => this.setState({selectedVideo}) }   
-              videos = {this.state.videos}/>
+   
+    return <div> Hello
           </div>
   }
 }
---
+++ book_list/src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+export default function () {
+    return [
+        { title: 'javascript books' },
+        { title: 'harry potter' },
+        { title: 'Dark tower' },
+        { title: 'Eloquent Ruby' }
+    ]
+}
+
+
+//step 1 create reducer 
+//step 2 wire into our application
